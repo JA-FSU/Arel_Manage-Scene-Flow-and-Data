e97dc03990c6c56756130b6f86672188e735578f
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2024-02-21 12:01:13 -0500

    Tutorials 2 and 3 Completed
    Finished the whole exercise with the last two tutorials.
    Fixed the bug from before as well.
    Customized the colors.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -25,6 +25,8 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -34,8 +36,21 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
+
         //Couldn't get the Unity Editor thing working.
         Application.Quit();
         Debug.Log("Quitting application!");
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
